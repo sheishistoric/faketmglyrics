631a2d23c217f8589dd5f9d79f8bf87109532154
Glitch (faketmglyrics) <none>
2018-06-06 17:31:45 +0000

    🙄🐺 Updated with Glitch

--- faketmglyrics.py
+++ faketmglyrics.py
@@ -2,9 +2,10 @@
 import os
 import time
 import os, ssl
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-    getattr(ssl, '_create_unverified_context', None)):
-    ssl._create_default_https_context = ssl._create_unverified_context
+print os.environ['cons_key']
+print os.environ['cons_secret']
+print os.environ['access_token']
+print os.environ['access_token_secret']
 
 from markovbot import MarkovBot
 #Initialize a MarkovBot instance
@@ -15,18 +16,8 @@ book = os.path.join(dirname, 'tmg copy.text')
 # Make your bot read the book!
 tweetbot.read(book)
 my_first_text = tweetbot.generate_text(25, seedword=['you', 'I am', 'going to', 'my', 'love',])
-print("tweetbot says:")
 print(my_first_text)
-# Consumer Key (API Key)
-cons_key = 'a'
-# Consumer Secret (API Secret)
-cons_secret = 'a'
-# Access Token
-access_token = 'a'
-# Access Token Secret
-access_token_secret = 'a'
-tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
+tweetbot.twitter_login(os.environ['cons_key'], os.environ['cons_secret'], os.environ['access_token'], os.environ['access_token_secret'])
 # Start periodically tweeting
-tweetbot.twitter_tweeting_start(days=0, hours=6, minutes=30, keywords=None, prefix=None, suffix=None)
-secsinweek = 7 * 24 * 60 * 60
-time.sleep(secsinweek)
+#tweetbot.twitter_tweeting_start(days=0, hours=0, minutes=5 keywords=None, prefix=None, suffix=None)
+tweetbot._t.statuses.update(status=my_first_text)
